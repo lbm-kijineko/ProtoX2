e2887c1fa687cf77d56c9654788082b243810642
shibata.shinichi <>
2022-10-27 22:29:21 +0900

    設定画面をフラグメントで作成

--- app/src/main/java/jp/co/lbm/protox2/ConfigurationActivity.java
+++ app/src/main/java/jp/co/lbm/protox2/ConfigurationActivity.java
@@ -1,7 +1,12 @@
 package jp.co.lbm.protox2;
 
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        // アクションバーに戻るを表示
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        return super.onOptionsItemSelected(item);
+
+        boolean returnVal = true;
+        int itemId = item.getItemId();
+        if(itemId == android.R.id.home) {
+            finish();
+        }
+        else {
+            returnVal = super.onOptionsItemSelected(item);
+        }
+        return returnVal;
+
     }
 }
